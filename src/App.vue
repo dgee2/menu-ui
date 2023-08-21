be7ca7ed96e2dc154fbaade073ee8056d4641eba
Daniel Gee <dgee2@users.noreply.github.com>
2023-08-21 19:46:24 +0100

    Add title

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <v-app>
     <v-app-bar flat color="primary">
       <v-app-bar-nav-icon :to="{ name: 'Home' }" icon="mdi-home" />
+      <v-app-bar-title>Menu</v-app-bar-title>
       <v-btn variant="text" :to="{ name: 'Recipes' }">Recipes</v-btn>
-
-      <v-spacer></v-spacer>
 
       <v-app-bar-nav-icon :to="{ name: 'CreateRecipe' }" icon="mdi-plus" />
     </v-app-bar>
+
     <v-main>
       <v-container>
         <router-view />
