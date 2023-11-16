d1def94fa54dd639520c60116a33e12cbeef30e7
Daniel Gee <dan@dan-gee.co.uk>
2023-11-16 11:08:11 +0000

    Fix datatable headers type after change to vuetify 3.4

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -8,14 +8,13 @@
 
 <script setup lang="ts">
 import { Recipe, getRecipes } from "@/services/menuApi";
-import { ReadonlyDataTableHeader } from "@/services/vuetifyHelperTypes";
+import { DataTableHeaders } from "@/services/vuetifyHelperTypes";
 import { onMounted, ref } from "vue";
-import { VDataTable } from "vuetify/labs/VDataTable";
 
 const loading = ref<boolean>(true);
 const recipes = ref<Recipe[]>();
 
-const headers: ReadonlyDataTableHeader[] = [
+const headers: DataTableHeaders = [
   {
     title: "Id",
     align: "end",
--- src/services/vuetifyHelperTypes.ts
+++ src/services/vuetifyHelperTypes.ts
@@ -1,8 +1,3 @@
-import { VDataTable } from "vuetify/lib/labs/VDataTable/index.mjs";
+import { VDataTable } from "vuetify/lib/components/VDataTable/index.mjs";
 
-type UnwrapReadonlyArrayType<A> = A extends Readonly<Array<infer I>>
-  ? UnwrapReadonlyArrayType<I>
-  : A;
-export type ReadonlyDataTableHeader = UnwrapReadonlyArrayType<
-  InstanceType<typeof VDataTable>["headers"]
->;
+export type DataTableHeaders = InstanceType<typeof VDataTable>["headers"];
